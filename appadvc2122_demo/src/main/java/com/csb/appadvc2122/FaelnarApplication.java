d018a11a054e58186533961cbd3a54342e68efa4
anjocortez <joseantonio.cortez@benilde.edu.ph>
2022-06-27 20:32:06 +0800

    Initial commit

---
+++ appadvc2122_demo/src/main/java/com/csb/appadvc2122/FaelnarApplication.java
@@ -0,0 +1,14 @@
+package com.csb.appadvc2122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class FaelnarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaelnarApplication.class, args);
+	}
+
+}
